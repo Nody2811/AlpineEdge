51900c935855da5996b4d8588339789e22e43162
Fadel Dia-Eddine <diaedfad@students.zhaw.ch>
2024-04-29 21:43:31 +0200

    new pictures

--- src/sections/Team/teamData.js
+++ src/sections/Team/teamData.js
@@ -1,7 +1,10 @@
-import profileImg2 from '../../img/team/fadel.jpeg'
+import profileImg2 from '../../img/team/Fadel.jpg'
 import profileImg1 from '../../img/team/rei.jpg'
 import profileImg3 from '../../img/team/Sascha.jpg'
 import profileImg4 from '../../img/team/Patrik.JPG'
+import profileImg5 from '../../img/team/Arsel.JPG'
+import profileImg6 from '../../img/team/Bega.JPG'
+import profileImg7 from '../../img/team/Daniele.JPG'
 const teamData = [
     {
         image: profileImg1,
@@ -16,14 +19,33 @@ const teamData = [
         text: "Projektleiter"
     },
     {
-        image: profileImg3,
-        name:  "Sascha Steiger",
+        image: profileImg5,
+        name:  "Arsel Arifi",
+        role: "",
+        text: "Assistent der Geschäftleitung"
+    },
+    {
+        image: profileImg6,
+        name:  "Rei Begaj",
+        role: "",
+        text: "Devops Ingenieur"
+    },
+    {
+        image: profileImg7,
+        name:  "Daniele Llazo",
+        role: "",
+        text: "Devops Ingenieur"
+    },
+  
+    {
+        image: profileImg4,
+        name:  "Patrik Kamberi",
         role: "",
         text: "Consultant"
     },
     {
-        image: profileImg4,
-        name:  "Patrik Kamberi",
+        image: profileImg3,
+        name:  "Sascha Steiger",
         role: "",
         text: "Consultant"
     },
